e33438c257b01ae5525ac4310e0559789cdacab5
nativegold <zxc6941@gmail.com>
2023-10-06 20:38:36 +0900

    Initial commit

---
+++ src/main/java/com/example/dolphintalkserver/DolphintalkServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dolphintalkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphintalkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphintalkServerApplication.class, args);
+    }
+
+}
