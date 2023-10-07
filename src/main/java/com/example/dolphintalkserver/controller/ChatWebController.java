b48f948146fea7e7543054d8f6fe2c3ab7d47a04
nativegold <zxc6941@gmail.com>
2023-10-07 22:33:05 +0900

    [UPDATE] updated build.gradle

---
+++ src/main/java/com/example/dolphintalkserver/controller/ChatWebController.java
@@ -0,0 +1,16 @@
+package com.example.dolphintalkserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatWebController {
+
+    @GetMapping("/chat/join")
+    public ResponseEntity<Void> join() {
+        return ResponseEntity.ok().build();
+    }
+}
